a4947d4c2776250b7930aff28747524df2a6524b
csamatov96 <csamatov96@gmail.com>
2019-12-18 15:28:32 -0600

    changes

--- variables.tf
+++ variables.tf
@@ -4,17 +4,17 @@ variable "region" {
      type = string
      description = "The AWS region."
    }
-   variable "key_name" {
-type = string
+variable "key_name" {
+    type = string
      description = "The AWS key pair to use for resources."
-   }
-   variable "ami" {
+}
+variable "ami" {
      type = map(string)
 #     type    = "map"
      description = "A map of AMIs."
      default = {}
    }
-   variable "instance_type" {
+variable "instance_type" {
      type = string
      description = "The instance type."
      default = "t2.micro"
