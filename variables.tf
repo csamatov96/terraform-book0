22afb12f8d71a49c7ec8ec0325dbbcdf83059973
csamatov96 <csamatov96@gmail.com>
2019-12-18 15:28:59 -0600

    changes

--- variables.tf
+++ variables.tf
@@ -1,21 +1,25 @@
-
-
 variable "region" {
      type = string
      description = "The AWS region."
-   }
-variable "key_name" {
-    type = string
-     description = "The AWS key pair to use for resources."
+     default     = "eu-west-1"
 }
+
+variable "region_list" {
+     #type = list(string)
+     description = "AWS availability zones."
+     default = ["eu-west-1a", "eu-west-1b"]
+}
+
 variable "ami" {
      type = map(string)
-#     type    = "map"
-     description = "A map of AMIs."
-     default = {}
-   }
-variable "instance_type" {
-     type = string
-     description = "The instance type."
-     default = "t2.micro"
+     default = {
+       eu-west-1 = "ami-01f14919ba412de34"
+     }
+     description = "The AMIs to use."
+}
+
+variable "security_group_ids" {
+     type    = list(string)
+     description = "List of security group IDs."
+     default = ["sg-4f713c35", "sg-4f713c35", "sg-4f713c35"]
 }
